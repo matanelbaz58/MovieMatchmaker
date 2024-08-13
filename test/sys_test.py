07bb53eba557ce06b9ad3e130ca7b567e9745d44
yaniv-simmer <simmeryaniv@gmail.com>
2024-08-13 22:12:52 +0300

    add tests for history storeing

--- test/sys_test.py
+++ test/sys_test.py
@@ -7,13 +7,12 @@ import os
 import pytest
 from deepdiff import DeepDiff
 
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from python_client import Client
 
 GENRE_LIST = {'Action': 28, 'Adventure': 12, 'Animation': 16, 'Comedy': 35, 'Crime': 80, 'Documentary': 99, 'Drama': 18, 'Family': 10751, 'Fantasy': 14, 'History': 36, 'Horror': 27, 'Music': 10402, 'Mystery': 9648, 'Romance': 10749, 'Science Fiction': 878, 'TV Movie': 10770, 'Thriller': 53, 'War': 10752, 'Western': 37}
-LANGUAGE_LIST = ['Aragonese', 'Akan', 'Cree', 'Azerbaijani', 'Czech', 'Afar', 'Breton', 'Afrikaans', 'Tibetan', 'Chechen', 'Cornish', 'Faroese', 'Latin', 'Ndonga', 'Sardinian', 'Tigrinya', 'Tswana', 'Turkish', 'Punjabi', 'Estonian', 'French', 'Hausa', 'Icelandic', 'Limburgish', 'Lingala', 'Swati', 'Abkhazian', 'Serbo-Croatian', 'Basque', 'Frisian', 'Japanese', 'Ojibwa', 'Oriya', 'Pali', 'Sundanese', 'Thai', 'Igbo', 'Indonesian', 'Kazakh', 'Kikuyu', 'Uighur', 'Venda', 'Kinyarwanda', 'Maori', 'Navajo', 'Hindi', 'Portuguese', 'Sango', 'Slovak', 'Serbian', 'Tahitian', 'Xhosa', 'Arabic', 'Corsican', 'Bislama', 'Esperanto', 'Herero', 'Finnish', 'Inuktitut', 'Latvian', 'Italian', 'Dutch', 'Kannada', 'Sanskrit', 'Albanian', 'Tagalog', 'Letzeburgesch', 'Tsonga', 'Malayalam', 'Volapük', 'Zulu', 'Ossetian; Ossetic', 'Samoan', 'Zhuang', 'Bengali', 'Slavic', 'Irish', 'Manx', 'Hungarian', 'Javanese', 'Kanuri', 'Khmer', 'Kirghiz', 'Nauru', 'Ndebele', 'Occitan', 'Romanian', 'Russian', 'Armenian', 'Chamorro', 'No Language', 'Bashkir', 'Galician', 'Ido', 'Luba-Katanga', 'Marshall', 'Malagasy', 'Moldavian', 'Mongolian', 'Ndebele', 'Norwegian', 'Polish', 'Swahili', 'Tajik', 'Tonga', 'Walloon', 'Yiddish', 'English', 'Assamese', 'Gaelic', 'Kalaallisut', 'Burmese', 'Quechua', 'Shona', 'Ukrainian', 'Persian', 'Georgian', 'Gujarati', 'Avaric', 'Avestan', 'Guarani', 'Maltese', 'Nepali', 'Swedish', 'Tatar', 'Wolof', 'Cantonese', 'Chuvash', 'Danish', 'Dzongkha', 'Chichewa; Nyanja', 'Rundi', 'Sotho', 'Turkmen', 'Uzbek', 'Vietnamese', 'Greek', 'Catalan', 'Welsh', 'German', 'Kashmiri', 'Malay', 'Norwegian Bokmål', 'Raeto-Romance', 'Sinhalese', 'Spanish', 'Telugu', 'Twi', 'Pushto', 'Bulgarian', 'Macedonian', 'Inupiaq', 'Korean', 'Lithuanian', 'Oromo', 'Northern Sami', 'Somali', 'Tamil', 'Urdu', 'Amharic', 'Bosnian', 'Divehi', 'Aymara', 'Bambara', 'Yi', 'Interlingue', 'Komi', 'Kurdish', 'Norwegian Nynorsk', 'Mandarin', 'Hebrew', 'Ewe', 'Fijian', 'Fulah', 'Haitian; Haitian Creole', 'Croatian', 'Interlingua', 'Kuanyama', 'Lao', 'Ganda', 'Marathi', 'Sindhi', 'Belarusian', 'Hiri Motu', 'Kongo', 'Slovenian', 'Yoruba'] 
+LANGUAGE_LIST = ['Abkhazian', 'Afar', 'Afrikaans', 'Akan', 'Albanian', 'Amharic', 'Arabic', 'Aragonese', 'Armenian', 'Assamese', 'Avaric', 'Avestan', 'Aymara', 'Azerbaijani', 'Bambara', 'Bashkir', 'Basque', 'Belarusian', 'Bengali', 'Bislama', 'Bosnian', 'Breton', 'Bulgarian', 'Burmese', 'Cantonese', 'Catalan', 'Chamorro', 'Chechen', 'Chichewa; Nyanja', 'Chuvash', 'Cornish', 'Corsican', 'Cree', 'Croatian', 'Czech', 'Danish', 'Divehi', 'Dutch', 'Dzongkha', 'English', 'Esperanto', 'Estonian', 'Ewe', 'Faroese', 'Fijian', 'Finnish', 'French', 'Frisian', 'Fulah', 'Gaelic', 'Galician', 'Ganda', 'Georgian', 'German', 'Greek', 'Guarani', 'Gujarati', 'Haitian; Haitian Creole', 'Hausa', 'Hebrew', 'Herero', 'Hindi', 'Hiri Motu', 'Hungarian', 'Icelandic', 'Ido', 'Igbo', 'Indonesian', 'Interlingua', 'Interlingue', 'Inuktitut', 'Inupiaq', 'Irish', 'Italian', 'Japanese', 'Javanese', 'Kalaallisut', 'Kannada', 'Kanuri', 'Kashmiri', 'Kazakh', 'Khmer', 'Kikuyu', 'Kinyarwanda', 'Kirghiz', 'Komi', 'Kongo', 'Korean', 'Kuanyama', 'Kurdish', 'Lao', 'Latin', 'Latvian', 'Letzeburgesch', 'Limburgish', 'Lingala', 'Lithuanian', 'Luba-Katanga', 'Macedonian', 'Malagasy', 'Malay', 'Malayalam', 'Maltese', 'Mandarin', 'Manx', 'Maori', 'Marathi', 'Marshall', 'Moldavian', 'Mongolian', 'Nauru', 'Navajo', 'Ndebele', 'Ndonga', 'Nepali', 'No Language', 'Northern Sami', 'Norwegian', 'Norwegian Bokmål', 'Norwegian Nynorsk', 'Occitan', 'Ojibwa', 'Oriya', 'Oromo', 'Ossetian; Ossetic', 'Pali', 'Persian', 'Polish', 'Portuguese', 'Punjabi', 'Pushto', 'Quechua', 'Raeto-Romance', 'Romanian', 'Rundi', 'Russian', 'Samoan', 'Sango', 'Sanskrit', 'Sardinian', 'Serbian', 'Serbo-Croatian', 'Shona', 'Sindhi', 'Sinhalese', 'Slavic', 'Slovak', 'Slovenian', 'Somali', 'Sotho', 'Spanish', 'Sundanese', 'Swahili', 'Swati', 'Swedish', 'Tagalog', 'Tahitian', 'Tajik', 'Tamil', 'Tatar', 'Telugu', 'Thai', 'Tibetan', 'Tigrinya', 'Tonga', 'Tsonga', 'Tswana', 'Turkish', 'Turkmen', 'Twi', 'Uighur', 'Ukrainian', 'Urdu', 'Uzbek', 'Venda', 'Vietnamese', 'Volapük', 'Walloon', 'Welsh', 'Wolof', 'Xhosa', 'Yi', 'Yiddish', 'Yoruba', 'Zhuang', 'Zulu']
 
 
 def test_direct_api_calls():
@@ -21,7 +20,14 @@ def test_direct_api_calls():
     Test the API calls that are called directly from the TMDB API without using the server as an intermediary.
     '''
     client = Client(True)
-    assert client.get_list_for_gui_dropdown('language') == LANGUAGE_LIST.sort()
+    assert client.get_list_for_gui_dropdown('language') == sorted(LANGUAGE_LIST)
+    assert client.get_list_for_gui_dropdown('genre') == sorted(list(GENRE_LIST.keys()))
+    assert client.get_list_for_gui_dropdown('sort_by') == ['popularity', 'release date', 'vote average']
+   
+    movie_recomend = client.get_movie_recommendations(user_input={'language': 'en', 'genre': 'comedy', 'with_cast': 'Tom Cruise'})
+    assert type(movie_recomend) == list
+    assert len(movie_recomend) == 10
+    
 
 
 def test_get_requests_from_server():
@@ -30,53 +36,47 @@ def test_get_requests_from_server():
     '''
     client = Client(True)
     assert client.get_genre_dict() == GENRE_LIST
-    movie_recomend = client.get_movie_recommendations(user_input={'language': 'en', 'genre': 'comedy'})
+
+
+def test_user_authentication():
+    '''
+    Test the registration of a new user and the removal of the same user.
+    '''
+    client = Client(True)
+    assert client.register_user('test_user', 'test_password') == True
+    assert client.register_user('test_user', 'test_password') == False
+
+    client_2 = Client(True)
+    assert client_2.login('rong_name', 'test_password') == 0
+    assert client_2.login('test_user', 'rong_password') == 2
+    assert client_2.login('test_user', 'test_password') == 1
+
+    assert client.remove_user('test_user', 'test_password') == True 
+    assert client.remove_user('test_user', 'test_password') == False
+
+    assert client_2.login('test_user', 'test_password') == 0
+
+def test_user_history():
+    '''
+    test the user history
+    '''
+    client = Client(True)
+    client.register_user('test_user', 'test_password')
+    assert client.get_user_history() == None
+    client.get_movie_recommendations(user_input={'language': 'en', 'genre': 'comedy', 'with_cast': 'Tom Cruise'}) 
+    assert client.get_user_history() == {'language': {'en': 1}, 'genre': {'comedy': 1}, 'with_cast': {'Tom Cruise': 1}}
+    client.get_movie_recommendations(user_input={'language': 'en', 'genre': 'comedy', 'with_cast': 'Leonardo DiCaprio'})
+    assert client.get_user_history() == {'language': {'en': 2}, 'genre': {'comedy': 2}, 'with_cast': {'Tom Cruise': 1, 'Leonardo DiCaprio': 1}}
+    client.get_movie_recommendations(user_input={'with_cast': 'Tom Cruise'})
+    assert client.get_user_history() == {'language': {'en': 2}, 'genre': {'comedy': 2}, 'with_cast': {'Tom Cruise': 2, 'Leonardo DiCaprio': 1}}
+    
+    movie_recomend = client.get_movie_recommendations_by_histoy()
     assert type(movie_recomend) == list
     assert len(movie_recomend) == 10
 
+    client.remove_user('test_user', 'test_password')
 
-# def test_user_authentication():
-#     '''
-#     Test the registration of a new user and the removal of the same user.
-#     '''
-#     client = Client(True)
-#     assert client.register_user('test_user', 'test_password') == True
-#     assert client.register_user('test_user', 'test_password') == False
-
-#     client_2 = Client(True)
-#     assert client_2.login('rong_name', 'test_password') == 0
-#     assert client_2.login('test_user', 'rong_password') == 2
-#     assert client_2.login('test_user', 'test_password') == 1
-
-#     assert client.remove_user('test_user', 'test_password') == True 
-#     assert client.remove_user('test_user', 'test_password') == False
-
-#     assert client_2.login('test_user', 'test_password') == 0
-
-import requests
-from io import BytesIO
-import tkinter as tk
-from PIL import Image, ImageTk
 if __name__ == '__main__':
-    #pytest.main()
-    x = Client(True).get_movie_recommendations(user_input={'language': 'en', 'genre': 'comedy'})
-    print(x[9]['poster_path'])
-    #print(Client(True).genre_dict)
-    # response = requests.get(f"{'https://image.tmdb.org/t/p/w185'}{x[0]['poster_path']}")
-    # img_data = response.content
-    
-    # root = tk.Tk()
-    # root.title("Movie Poster")
-    
-    # # Load image data into PIL and then convert it to a format tkinter can use
-    # img = Image.open(BytesIO(img_data))
-    # img_tk = ImageTk.PhotoImage(img)
-
-    # # Create a label and add the image to it
-    # label = tk.Label(root, image=img_tk)
-    # label.image = img_tk  # Keep a reference to avoid garbage collection
-    # label.pack()
-
-    # # Run the tkinter loop
-    # root.mainloop()
+    pytest.main()
+    #test_user_history()
 
