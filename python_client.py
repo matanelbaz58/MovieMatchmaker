f118a89a6726ce3e3413b5567390c776a7bdb72e
yaniv-simmer <simmeryaniv@gmail.com>
2024-08-17 23:22:59 +0300

    improver documentation

--- python_client.py
+++ python_client.py
@@ -1,9 +1,10 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 import requests
 from web3_utils import Web3UserHistoryHandler
 
 SERVER_URL = "http://localhost:5000"
 TMDB_URL = "https://api.themoviedb.org/3"
+SEPOLIA_URL = "https://sepolia.etherscan.io/tx/"
 TMDB_AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjJhNDA5ZTJhOWM2NmYyNDVhMGIzZDIyMzE3OTIyMiIsInN1YiI6IjY1ZGNmMzUyOGMwYTQ4MDEzMTFkYTI0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1_XHPeZXtKSrozDmPcZKEaIbz4W5CpfloqD0l0LDLtY"
 SORT_BY_OPTIONS = {'popularity' : 'popularity.desc', 'release date' : 'releasedate.desc', 'vote average' : 'vote_average.desc'}
 
@@ -15,9 +16,14 @@ class Client:
         self.genre_dict = self.get_genre_dict()
         self.language_dict = self.get_language_dict()
         self.web3_history_manager = Web3UserHistoryHandler() if not is_mongo_db_client else None
+        #TODO: add monoDB self.history_manager session !
 
     
     def is_MongoDB_client(self):
+        '''
+        Returns:
+            bool: True if the client is a MongoDB client, False if the client is a web3 client.
+        '''
         return self.web3_history_manager == None
 
 
@@ -26,8 +32,8 @@ class Client:
         Logs in the user.
 
         Args:
-            user_name (str): The user's name.
-            user_password (str): The user's password.
+            public_identifier (str): The user's name / wallet address.
+            private_identifier (str): The user's password / wallet private key.
 
         Returns:
             0 (int): if the user name does not exist.
@@ -57,11 +63,19 @@ class Client:
 
     def register_user(self, public_identifier: str, private_identifier: str) -> bool:
         """
-        
+        Registers a new user. And logs in the user.
+
+        Args:
+            public_identifier (str): The user's name / wallet address.
+            private_identifier (str): The user's password / wallet private key.
+
+        Returns:
+            bool: True if the user was registered successfully
         """
         #TODO: return rc indicating the reason for failure
         if self.is_MongoDB_client():
-            response = requests.post(f"{SERVER_URL}/register_mongoDB", json={"user_name": public_identifier, "user_password": private_identifier})
+            response = requests.post(f"{SERVER_URL}/register_mongoDB", json={"user_name": public_identifier,
+                                                                             "user_password": private_identifier})
             self.login(public_identifier, private_identifier)
             return response.json()['success']
         else:
@@ -71,13 +85,15 @@ class Client:
     def remove_user(self) -> dict:
         """
         Removes a user from the database.
-        If the user is a web3 client, will return False anyway(user cannot be removed from the blockchain).
-        
+        If the user is a web3 client, will return False and and clear the user's history. (False because user cannot be removed from the blockchain)
+          
         Returns:
             True : if the user was removed successfully.
             False : if the user name does not exist or failed to remove.
         """
-        if not self.is_MongoDB_client(): return False # User cannot be removed from the blockchain
+        if not self.is_MongoDB_client():
+            self.clear_user_history() #TODO: test this on sys_test.pt
+            return False 
         response = requests.post(f"{SERVER_URL}/remove_user_from_mongoDB", json={"user_name": self.public_identifier, "user_password": self.private_identifier})
         return response.json()['success']
 
@@ -87,7 +103,7 @@ class Client:
         Fetches a list of items for a dropdown menu in the GUI.
 
         Args:
-            field (str): The field for which to fetch the list.
+            field (str): The field for which to fetch the list. Options are :'genre', 'language', 'sort_by'
 
         Returns:
             list: A list of items for the dropdown menu.
@@ -105,10 +121,10 @@ class Client:
         Prases user input to the format required by the TMDB API.
 
         Args:
-            user_input (dict): A dictionary containing user preferences.
+            user_input (dict): A dictionary containing user input.
 
         Returns:
-            dict: A dictionary containing user preferences in the format required by the TMDB API.
+            dict: A dictionary containing user input in the format required by the TMDB API.
         """
         for key in user_input.keys():
             if key == 'language':
@@ -122,13 +138,13 @@ class Client:
         return user_input
 
     
-    def get_movie_recommendations(self, user_input, poster_image_size=500, store_data=True) -> Tuple[List[dict], str]:
+    def get_movie_recommendations(self, user_input, poster_image_size=500, store_data=True) -> Tuple[List[dict], Optional[str]]:
         """
         Fetch movie recommendations based on user input.
 
         Parameters:
             poster_image_size (int): The size of the poster image to fetch. default is 500.
-                other options are 92, 154, 185, 342, 500, 780.
+                other options are: 92, 154, 185, 342, 500, 780.
         
             user_input (dict): 
                 A dictionary containing user preferences. if a key is missing, the default value is used.
@@ -173,65 +189,43 @@ class Client:
 
                 
         Returns:
-            A list of 10 dictionaries. each dictionarie contains data about a movie recommendation.
-            Each dictionarie contains the next keys:
-                'backdrop_path' (path to a backround imaage, could be used for text backround)
-                'genre_ids'
-                'title'
-                'original_language'
-                'overview' (a short description of the movie)
-                'popularity'
-                'poster_path' (path to the movie's main cover  image)
-                'release_date'
-                'title'
+            Tuple[List[dict], Optional[str]]: A tuple containing a list of movie recommendations and a link to the transaction hash on Etherscan.
+                The list contains dictionaries with movie data. Each dictionary contains the following keys:
+                    - title (str): The title of the movie. #TODO: should this be original_title?
+                    - release_date (str): The release date of the movie.
+                    - genre_ids (list): A list of genres of the movie.
+                    - overview (str): A brief description of the movie.
+                    - popularity (float): The popularity of the movie.
+                    - original_language (str): The original language of the movie.
+                    - backdrop_path (str): The URL of the backdrop image of the movie. (could be used for text backround)
+                    - cast (list): A list of main actors in the movie. #TODO: this isnt implemented yet
+                    
+                The link to the transaction hash on Etherscan is only returned if the client is a web3 client and store_data is True.
             
             .Returns None if the GET request fails.
 
             
         Example return value:
-            [
+            ([
                 {
                     'title': 'Movie Title 1',
                     'release_date': '2023-01-01',
-                    'poster_path': '\\x81\\x0b\\x7 ...',
                     'genre_ids': ['Comedy', 'Family']
                     ...
                 },
                 {
                     'title': 'Movie Title 2',
                     'release_date': '2023-02-01',
-                    'poster_path': '\\x81\\x0b\\x7 ...',
                     ...
                 },
                 ...
-            ]         
+            ],
+            'https://sepolia.etherscan.io/tx/0x123456...')         
 
               
         Example python usage:
-            user_input = {'user_id': 123, 'genre': 'comedy'}
-            recommendations = get_movie_recommendations(user_input)
-        
-        
-        Example of poster_path image showing with tkinter:
-            import requests
-            from io import BytesIO
-            import tkinter as tk
-            from PIL import Image, ImageTk
-
-            responce = Client(True).get_movie_recommendations(user_input={'language': 'en', 'genre': 'comedy'}, poster_image_size=185)
-            img_data = img_data[0]['poster_path']
-            root = tk.Tk()
-            root.title("Movie Poster")
-    
-            # Load image data into PIL and then convert it to a format tkinter can use
-            img = Image.open(BytesIO(img_data))
-            img_tk = ImageTk.PhotoImage(img
-            # Create a label and add the image to it
-            label = tk.Label(root, image=img_tk)
-            label.image = img_tk  # Keep a reference to avoid garbage collection
-            label.pack(
-            # Run the tkinter loop
-            root.mainloop()
+            user_input = {'genre': 'Comedy', 'language': 'English'}
+            recommendations = client.get_movie_recommendations(user_input)
         """
         #TODO: Add a list of main actors cast to the output
         if store_data:
@@ -242,18 +236,18 @@ class Client:
         response = requests.get(url, params=user_input)
 
         if response.status_code != 200:
-            return response
+            return None
         else:
             data = response.json()['results'][:10]           
             movie_resaults = [self.format_tmdb_results(movie_dict, poster_image_size) for movie_dict in data]
             if not self.is_MongoDB_client() and store_data:
-                return movie_resaults, f"https://sepolia.etherscan.io/tx/{tx_hash}"
+                return movie_resaults, SEPOLIA_URL + tx_hash 
             return movie_resaults
                 
         
     def format_tmdb_results(self, data: dict, poster_image_size) -> dict:
         """
-        Formats the data fetched from the TMDB API.
+        Formats the data fetched from the TMDB API to a more readable user-friendly format.
 
         Parameters:
             data (dict): A dictionary containing movie data fetched from the TMDB API.
@@ -276,27 +270,25 @@ class Client:
                 return_dict[key] = data[key]
                 
         return return_dict
- 
-            
+          
 
     def get_genre_dict(self) -> dict[str, int]:
         '''
-        Fetches a list of movie genres from the API.
+        Fetches a dictionary of movie genres from the API.
         
         returns:
             dict: A dictionary containing movie genres and their corresponding IDs.      
+            None: If the GET request fails.
         '''
         url = f"{SERVER_URL}/get_genre_dict"
         response = requests.get(url)
-        if response.status_code != 200:
-            return None
-        else:
-            return response.json()
+        return response.json() if response.status_code == 200 else None
 
 
     def get_language_dict(self) -> dict:
         """
-        Fetches a dictionary 
+        Fetches a dictionary of languages from the API.
+
         Returns:
             dict: A dictionary containing languages and their corresponding ISO 639-1 codes.
         """
@@ -312,7 +304,6 @@ class Client:
             return None
         data = response.json()
         return {language['english_name']: language['iso_639_1'] for language in data}
-
 
 
     def get_movie_images(self, movie_title: int) -> dict:
@@ -338,7 +329,16 @@ class Client:
     
 
     def update_user_preferences(self, user_input: dict, user_history: dict) -> dict:
+        '''
+        Updates the user's search history with the new input. 
 
+        Parameters:
+            user_input (dict): A dictionary containing the user's input.
+            user_history (dict): A dictionary containing the user's search history.
+        
+        Returns:
+            dict: The updated user's search history.
+        '''
         for key, value in user_input.items():
             if key not in user_history:
                 user_history[key] = {value: 1}
@@ -348,9 +348,18 @@ class Client:
                 user_history[key][value] += 1
         return user_history
 
+
     def store_input_to_history(self, user_input: dict) -> bool | str:
         """
-        Stores the user's search history in MongoDB.
+        Stores the user's search history in MongoDB or the blockchain. 
+
+        Parameters:
+            user_input (dict): A dictionary containing the user's input.
+        
+        Returns:
+            bool: True if the user's search history was stored successfully.
+            str: The transaction hash if the client is a web3 client.
+
         """
         # TODO: make sure language is stored correctly
         
@@ -367,10 +376,19 @@ class Client:
     
     def get_user_history(self) -> dict:
         """
-        Retrieves the user's search history from MongoDB.
+        Fetches the user's search history from MongoDB or the blockchain.
 
+        
         Returns:
             dict: The user's search history.
+        
+            
+        Example return value example:
+            {
+                'language': {'English': 1},
+                'genre': {'Comedy': 2, 'Family': 1},
+                'with_cast': {'Tom Cruise': 1}
+            }
         """
         if self.is_MongoDB_client():
             url = f"{SERVER_URL}/get_user_history_from_mongoDB"
@@ -379,10 +397,12 @@ class Client:
         
         return self.web3_history_manager.get_user_history(self.public_identifier)
 
-    def get_movie_recommendations_by_histoy(self):
-        """
-        Fetch movie recommendations based on user history.
 
+    def get_movie_recommendations_by_histoy(self) -> Tuple[List[dict], Optional[str]]:
+        """
+        Fetch movie recommendations based on user search history.
+
+        
         Returns:
             same as get_movie_recommendations()
         """
@@ -396,7 +416,7 @@ class Client:
             
     def clear_user_history(self) -> bool:
         """
-        Clears the user's search history from MongoDB.
+        Clears the user's search history from MongoDB or the blockchain.
         
         Returns:
             bool: True if the user's search history was cleared successfully.
@@ -405,5 +425,5 @@ class Client:
             url = f"{SERVER_URL}/clear_user_history_from_mongoDB"
             response = requests.get(url, params={"user_name": self.public_identifier})
             return response.status_code == 200
-        
+
         return self.web3_history_manager.clear_user_history(self.public_identifier ,self.private_identifier)
